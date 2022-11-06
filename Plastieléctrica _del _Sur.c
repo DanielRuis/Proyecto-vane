00697ea3704b3c5498abc44230d4ba02ea4f419f
DanielRuis <danieleruisl225@gamail.com>
2022-11-05 22:58:41 -0600

    Muestra de forma correcta a un cliente, se simplifico el showClientes

---
+++ Plastieléctrica _del _Sur.c
@@ -0,0 +1,172 @@
+//?No debe de tener ni una interaccion con la base de datos
+// gcc -o Pla Plastieléctrica _del _Sur.c -I /usr/include/postgresql/ -lpq
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <time.h>
+//*inicializacion de lasa funciones
+int menuCliente();
+int menuArticulos();
+int menuVentas();
+int showClientes();
+int showOneCliente();
+int addCliente();
+int updateCliente();
+int deleteCliente();
+
+//*inicializacion de variables
+int fd1,fd2,fde=0,i,x;
+double tiempo=0.0;
+char txt[2];
+//!Funcion principal
+int main(){
+    int menu;
+    while(1){
+        //remove("/home/dano/Documentos/7mo/SO/ProyectoFinal/listaclientes2");
+        //*menu a presentar al cliente
+        printf("\n      Plastieléctrica del Sur       \n");
+        printf("------------------------------------\n");
+        printf("                MENU                \n");
+        printf("------------------------------------\n");
+        printf("|1-Clientes                        |\n");
+        printf("|2-Articulos                       |\n");
+        printf("|3-Ventas                          |\n");
+        printf("|4-Salir                           |\n");
+        printf("------------------------------------\n");
+        scanf("%i",&menu);
+        //? El switch nos lleva al segundo menu
+        switch(menu){
+            //*clientes
+            case 1:
+                //printf("Entrando a la seccion de clientes\n");
+                menuCliente();               
+            break;
+            //*Articulos
+            case 2:
+                //menuArticulos();
+            break;
+            //*Ventas
+            case 3:
+                //menuVentas();
+            break;
+            //*Salir
+            case 4:
+                printf("Saliendo...\n");
+                exit(EXIT_SUCCESS);
+            break;
+            default:
+                printf("No tenemos esa opcion, vuelva a intentarlo\n");
+            break;
+        }
+    }
+}
+
+int menuCliente(){
+    int menu;
+    char txt[5];
+    clock_t begin;
+    clock_t end;
+    fd2=open("menu1",O_WRONLY);
+    printf("------------------------------------\n");
+    printf("                CLIENTE             \n");
+    printf("------------------------------------\n");
+    printf("|1-Lista de clientes               |\n");
+    printf("|2-Lista de clientes con credito   |\n");
+    printf("|3-Datos de un cliente             |\n");
+    printf("|4-Añadir                          |\n");
+    printf("|5-Actualizar                      |\n");
+    printf("|6-Borrar                          |\n");
+    printf("|7-Salir                           |\n");
+    printf("------------------------------------\n");
+    scanf("%i",&menu);
+    fd2=open("menu1",O_WRONLY);
+    switch(menu){
+        
+        //*Lista de clientes 
+        case 1:
+            begin=clock();
+            sprintf(txt,"1");
+            write(fd2,txt,sizeof(txt));
+            close(fd2);
+            showClientes();
+            end=clock();
+            tiempo+=(double)(end-begin)/CLOCKS_PER_SEC;
+        break;
+        case 2:
+            begin=clock();
+            sprintf(txt,"2");
+            write(fd2,txt,sizeof(txt));
+            close(fd2);
+            showClientes();
+            end=clock();
+            tiempo+=(double)(end-begin)/CLOCKS_PER_SEC;
+        break;
+        //*Datos de un cliente
+        case 3:
+            begin=clock();
+            sprintf(txt,"3");
+            write(fd2,txt,sizeof(txt));
+            close(fd2);
+            showOneCliente();
+            end=clock();
+            tiempo+=(double)(end-begin)/CLOCKS_PER_SEC;
+        break;
+        //*añadir
+        case 4:
+            
+        break;
+        //*Actualizar
+        case 5:
+            
+        break;
+        //*borrar
+        case 6:
+
+        break;
+    }
+    
+        printf("\n-----------------------\nTiempo de ejecucion:%f\n", tiempo);
+
+}
+
+//?Mostrar clientes
+int showClientes(){
+    char cad[2024];
+    fd2=open("lista1",O_RDONLY);
+    read(fd2,cad,2024);
+    close(fd2);
+    printf("--------------------CLIENTES--------------------------\n");
+    printf("%s\n",cad);
+    
+}
+//?Se muestra datos de un solo cliente
+showOneCliente(){
+    char idcad[10],cad[2024];
+    int idcli;
+    sprintf(idcad,"");
+    
+    printf("Inserte el id del cliente:");
+    scanf("%i",&idcli);
+    sprintf(idcad,"%i",idcli);
+
+    fd2=open("IdCliente",O_WRONLY);
+    write(fd2,idcad,sizeof(idcad));
+    close(fd2);
+
+    fd1=open("lista2",O_RDONLY);
+    read(fd1,cad,sizeof(cad));
+    close(fd1);
+    printf("--------------------DATOS DEL CLIENTE %i--------------------------\n",idcli);
+    printf("%s\n",cad);
+
+}
+
+addCliente(){
+    
+}
+
+
--- Server.c
+++ Server.c
@@ -11,19 +11,151 @@ PGresult *result = NULL;
 //?Variables para la conexion a la bd
 char *host = "localhost";
 char *port = "5432";
-char *dataBase = "ferreteria";
+char *dataBase = "electronica";
 char *user = "postgres";
 char *passwd = "31342522";
-//?variables globales
-int i,j,fd1,fd2,sqldata;
+//*variables globales
+int i,j,fd1,fd2,sqldata,menu;
 char txt[5],sql[1234];
+//*funciones:Cliente
+int addCliente();
+int updateCliente();
+int showCliente();
+int deleteCliente();
+int showAllCliente();
+int showOneCliente();
 // gcc -o Server Server.c -I /usr/include/postgresql/ -lpq
 //!Funcion principal
 int main(){
-    con = PQsetdbLogin(host,port,NULL,NULL,dataBase,user,passwd);//*abro conexion   
-    if(PQstatus(con) != CONNECTION_BAD){
-        printf("Iniciando...\n");
-    }else{
-        printf("Fallo en la conexion\n");
+    while(1){
+        //*borramos los archivos 
+        remove("/home/dano/Documentos/7mo/SO/ProyectoFinal/lista1");
+        remove("/home/dano/Documentos/7mo/SO/ProyectoFinal/menu");
+        mkfifo("lista1",0666);
+        mkfifo("menu1",0666);//*->Abrimos el fifo para el menu de acciones
+        fd1=open("menu1",O_RDONLY);
+        read(fd1,txt,5);//*->Leemos y se guarda en la variable txt
+        close(fd1);
+        menu=atoi(txt);
+        //? Switch de acciones
+        switch(menu){
+            //!Seccion de clientes del 1-5
+            case 1:
+                
+                printf("Entra a mostrar clientes\n");
+                showAllCliente(menu);//*->Mostrar lista de clientes detallado
+            break;
+            case 2:
+                printf("Entrando a mostar clientes con credito\n");
+                showAllCliente(menu);
+            break;
+            case 3:
+                showOneCliente();//*->Mostrar a un solo cliente
+            break;
+            case 4:
+                //addCliente();//*->Añadir Cliente
+            break;
+            case 5:
+                //showCliente();
+                //updateCliente();//*->Actualizar cliente
+            break;
+            case 6:
+                //deleteCliente();//*->Borrar cliente
+            break;
+        }
+
+
     }
+}
+
+//! ------------------CLIENTES--------------------------------------------------------
+//? Muestra todos los clientes
+int showAllCliente(int m){
+    char bf[900],cad[2024];
+    int ls;
+    sprintf(cad,"");
+    printf("Servicio de mostrar a todos los clientes\n");
+    //*Se inicia conexion
+    con = PQsetdbLogin(host,port,NULL,NULL,dataBase,user,passwd);//*abro conexion   
+    //?Decide que consulta hacer
+    switch(m){
+        case 1:
+            result=PQexec(con, "select * from clientes");
+        break;
+        case 2:
+            result=PQexec(con, "select id_cliente, nombre, credito, deuda from clientes where id_typecli=2");
+            strcat(cad,"ID\t\tNOMBRE\t\tCRED($)\t\tDEUDA($)\n\n");
+        break;
+    }
+    printf("Filas:%d, Columnas:%d \n",PQntuples(result),PQnfields(result));
+    mkfifo("lista1",0666);//*Creamos el FIFO
+    fd2=open("lista1",O_WRONLY);//*Abrimos para escritura
+    //?Entramos a la lista de datos a mostrar
+    if(PQntuples(result)!=0){
+        i=0;
+        for (i = 0; i < PQntuples(result); i++){ 
+            for (j = 0; j < PQnfields(result); j++){
+                sprintf(bf,"%s\t\t",PQgetvalue(result,i,j));
+                strcat(cad,bf);   
+            } 
+            strcat(cad,"\n");
+        }
+        //printf("%s\n",cad);
+        write(fd2,cad,sizeof(cad));//!Se escribe y se manda al cliente
+    }else{
+        //?Entra si no hay clientes en la base de datos
+        sprintf(cad,"No hay clientes\n");
+        printf("%s",cad);
+        write(fd2,cad,sizeof(cad));//!Escribe que no hay clientes
+    }
+    close(ls);//*Cerramos
+    PQfinish(con);
+}
+
+//?Servicio de mostrar un solo cliente
+//*Se recibe el id del cliente
+//*Solo aqui se mostrara la lista de clientes pero cliente no lo va a ver
+int showOneCliente(){
+    mkfifo("IdCliente",0666);//*Creamos el fifo
+    mkfifo("lista2",0666);//*Creamos el FIFO
+    char sqlcad[1100],cad[2024],idcad[10],bf[900];
+    sprintf(cad,"");
+    sprintf(idcad,"");
+    sprintf(sqlcad,"");
+    sprintf(bf,"");
+    printf("Servicio de mostrar a todos los clientes\n");
+
+    //*Se inicia conexion
+    con = PQsetdbLogin(host,port,NULL,NULL,dataBase,user,passwd);//*abro conexion   
+
+    //*abrimos canal de comunicacion de lectura
+    fd2=open("IdCliente",O_RDONLY);
+    read(fd2,idcad,sizeof(idcad));//*->Hacemos lectura dek id
+    close(fd2);//*Cerramos el canal fd2
+    //printf("%s\n",idcad);
+
+    //*Se realiza la instruccion sql
+    sprintf(sqlcad,"Select * from clientes where id_cliente='%s'",idcad);
+    printf("%s\n",sqlcad);
+    result=PQexec(con,sqlcad);
+    fd1=open("lista2",O_WRONLY);//*Abrimos como escritura con fd1
+    //*Se junta los datos
+    if(PQntuples(result)!=0){
+        //?Si encuentra al cliente entra
+        
+        for (j = 0; j < PQnfields(result); j++){
+            sprintf(bf,"%s\t\t",PQgetvalue(result,0,j));
+            strcat(cad,bf);   
+        } 
+        strcat(cad,"\n");
+        //printf("%s\n",cad);
+        write(fd1,cad,sizeof(cad));//!Se escribe y se manda al cliente
+    }else{
+        //?Entra si no está el cliente buscado
+        sprintf(cad,"No se encuentra el cliente en la lista\n");
+        printf("%s",cad);
+        write(fd1,cad,sizeof(cad));//!Escribe que no hay clientes
+    }
+    close(fd1);//*Cerramos a fd1
+    PQfinish(con);//*Cerramos conexion
 }
